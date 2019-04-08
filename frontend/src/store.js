0a3af410ba12280335debdb15d077fe5caf175ce
Tran Nguyen <tcdnguyen1997@gmail.com>
2019-04-08 21:46:31 +0700

    deploy-3

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -7,9 +7,14 @@ const initialState = {};
 
 const middlewares = [thunk];
 
-const store = createStore(RootReducer, initialState, compose(
-    applyMiddleware(...middlewares),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-));
+// const store = createStore(RootReducer, initialState, compose(
+//     applyMiddleware(...middlewares),
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// ));
+const composeEnhancers =  typeof window === 'object' &&  window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
 
+const composingMiddlewareAndDevTools = composeEnhancers(applyMiddleware(...middlewares));
+
+const store = createStore(RootReducer, initialState, composingMiddlewareAndDevTools);
 export default store;
