5328a4262760cbf97e9de2087ecc1599a151a48e
Tran Nguyen <tcdnguyen1997@gmail.com>
2019-05-25 14:32:34 +0700

    fix hiex

--- frontend/src/components/TriangleBtn.js
+++ frontend/src/components/TriangleBtn.js
@@ -14,14 +14,14 @@ export default class TriangleBtn extends Component {
   };
   render() {
     let {value, hiex} = this.props;
-    let amount = value*20;
+    let amount = hiex*20;
     let pointGroup = `36,${309-amount} 36,${325-amount} 48,${317-amount}`;
     return (
       <div className="triangle-btn-box">
         <svg width="50" height="340"  >
             <polygon className="triangle" points={pointGroup} onClick={this.props.onOpenModal}></polygon>
         </svg>
-        <div className="hiexp" style={{top: `${19 - value*1.25}em`}}>{hiex}</div>
+        <div className="hiexp" style={{top: `${19 - hiex*1.25}em`}}>{hiex}</div>
       </div>
     )
   }
